f44acca9c061028926908bc9a415e973a8272ed8
someguy2 <nathanchan4121@gmail.com>
2022-03-27 17:31:04 -0400

    Added skill checks

--- Main.py
+++ Main.py
@@ -78,13 +78,45 @@ def hp_stuff():
             currentHp += int(change)
     print("New Hp: " + str(currentHp))
 
+def roll_save():
+    typeDict = {
+        '1': 'fort', #night
+        '2': 'reflex',
+        '3': 'will'
+    }
+    type = input("What type of save? \n 1) Fortitude \n 2) Reflex \n 3) Will\n")
+    roll = random.randrange(1, 21)
+    bonus = data['saves'][typeDict[type]]['total']
+    print(str(roll) + " + " + str(bonus) + " = " +  str(int(roll) + int(bonus)))
+
+def roll_skill():
+    typeDict = {
+        '1': 'acrobatics', #night
+        '2': 'intimidate',
+        '3': 'bluff',
+        '4': 'stealth',
+        '5': 'appraise',
+        '6': 'swim',
+        '7': 'ride'
+    }
+    print("What type of check?")
+    for i, va in typeDict.items():
+        print(i + ") " + va)
+    type = input("")
+    roll = random.randrange(1, 21)
+    bonus = data['skills'][typeDict[type]]['total']
+    print(str(roll) + " + " + str(bonus) + " = " +  str(int(roll) + int(bonus)))
+
+
 def start():
     commandDict = {
         "1": m_attack_roll,
         "2": r_attack_roll,
         "3": get_ac,
         "4": roll_intiative,
-        "5": hp_stuff
+        "5": hp_stuff,
+        "6": roll_save,
+        "7": roll_skill
     }
     print("Character: " + data['name'])
     while(True):
